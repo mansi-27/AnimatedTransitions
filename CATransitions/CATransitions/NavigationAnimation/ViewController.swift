1688850a1a60bdbfbd4c7b43a68f5a6e3dea0071
Mansi Shah <mansi.iosdev@gmail.com>
2017-12-16 23:47:25 +0530

    Added comments and view background color

--- CATransitions/CATransitions/NavigationAnimation/ViewController.swift
+++ CATransitions/CATransitions/NavigationAnimation/ViewController.swift
@@ -10,24 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
     @IBOutlet weak var btnNavigate: UIButton!
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
         btnNavigate.setTitle("Navigate", for: .normal)
+        view.backgroundColor = MyColorHelper.darkBlue
     }
     
-    @IBAction func btnNavigateTapped(_ sender: UIButton) {
-        // 👉🏼 Add your animation before you push/pop the view controller
-        navigationController?.addTransition()
-        // 👉🏼 Don't forget to set the default animated property to FALSE
-        navigationController?.pushViewController(SecondViewController(), animated: false)
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - IBActions
+    @IBAction func btnNavigateTapped(_ sender: UIButton) {
+        /// 👉🏼 Add your animation before you push/pop the view controller
+        navigationController?.addTransition()
+        /// 👉🏼 Don't forget to set the default animated property to FALSE
+        navigationController?.pushViewController(SecondViewController(), animated: false)
+    }
 }
 
