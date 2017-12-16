221e6ca12c64afdad15a95ef668045f0868e866b
Mansi Shah <mansi.iosdev@gmail.com>
2017-12-16 23:46:18 +0530

    Added 2 Buttons for presenting with flip and curl animations

--- CATransitions/CATransitions/NavigationAnimation/SecondViewController.swift
+++ CATransitions/CATransitions/NavigationAnimation/SecondViewController.swift
@@ -10,24 +10,43 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    // MARK: - IBOutlets
     @IBOutlet weak var btnGoBack: UIButton!
+    @IBOutlet weak var btnHorizontalFlip: UIButton!
+    @IBOutlet weak var btnPageCurl: UIButton!
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         btnGoBack.setTitle("Go Back", for: .normal)
+        btnHorizontalFlip.setTitle("Present with a Flip", for: .normal)
+        btnPageCurl.setTitle("Present with a Curl", for: .normal)
         navigationController?.navigationBar.isHidden = true
-        view.backgroundColor = UIColor.gray.withAlphaComponent(0.3)
-    }
-    
-    @IBAction func btnGoBackTapped(_ sender: UIButton) {
-        // 👉🏼 Add your animation before you push/pop the view controller
-        navigationController?.addTransition()
-        // 👉🏼 Don't forget to set the default animated property to FALSE
-        navigationController?.popViewController(animated: false)
+        view.backgroundColor = MyColorHelper.green
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - IBActions
+    @IBAction func btnGoBackTapped(_ sender: UIButton) {
+        /// 👉🏼 Add your animation before you push/pop the view controller
+        navigationController?.addTransition()
+        /// 👉🏼 Don't forget to set the default animated property to FALSE
+        navigationController?.popViewController(animated: false)
+    }
+    
+    @IBAction func btnHorizontalFlipTapped(_ sender: UIButton) {
+        let presentationVC = ThirdViewController()
+        presentationVC.modalTransitionStyle = .flipHorizontal
+        present(presentationVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func btnPageCurlTapped(_ sender: UIButton) {
+        let presentationVC = ThirdViewController()
+        presentationVC.modalTransitionStyle = .partialCurl
+        present(presentationVC, animated: true, completion: nil)
     }
 }
