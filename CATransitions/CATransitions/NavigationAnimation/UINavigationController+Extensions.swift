c8a7bb0d500d42a7cec2f102caf27300e126f5bd
Mansi Shah <mansi.iosdev@gmail.com>
2017-12-16 23:47:00 +0530

    Changed animation time to 1.0

--- CATransitions/CATransitions/NavigationAnimation/UINavigationController+Extensions.swift
+++ CATransitions/CATransitions/NavigationAnimation/UINavigationController+Extensions.swift
@@ -11,18 +11,18 @@ import UIKit
 extension UINavigationController {
     
     func addTransition() {
-        // First, we initiate a CATransition object
+        /// First, we initiate a CATransition object
         let transition = CATransition()
-        // Second, we define the duration for the transition to get COMPLETED
-        transition.duration = 1.5
-        // Here, you define the animation pacing (whether it starts slowly, ends faster OR starts faster, ends slowly... etc)
+        /// Second, we define the duration for the transition to get COMPLETED
+        transition.duration = 1.0
+        /// Here, you define the animation pacing (whether it starts slowly, ends faster OR starts faster, ends slowly... etc)
         transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        // You can use kCATransitionMoveIn, kCATransitionFade, kCATransitionPush, or kCATransitionReveal as the transition type 🤗
+        /// You can use kCATransitionMoveIn, kCATransitionFade, kCATransitionPush, or kCATransitionReveal as the transition type 🤗
         transition.type = kCATransitionPush
-        // You can use kCATransitionFromBottom, kCATransitionFromTop, kCATransitionFromLeft, or kCATransitionFromRight as the transition sub-type 🤗
+        /// You can use kCATransitionFromBottom, kCATransitionFromTop, kCATransitionFromLeft, or kCATransitionFromRight as the transition sub-type 🤗
         transition.subtype = kCATransitionFromTop
-        // Lastly, we add this animated transition to the layer of Navigation controller.
-        // 👉🏼 Make sure, this is added before actually pushing/ pop-ing the next/previous view controller
+        /// Lastly, we add this animated transition to the layer of Navigation controller.
+        /// 👉🏼 Make sure, this is added before actually pushing/ pop-ing the next/previous view controller
         self.view.layer.add(transition, forKey: nil)
     }
 }
